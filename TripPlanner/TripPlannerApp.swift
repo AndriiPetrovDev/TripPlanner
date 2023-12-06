c753b71bacd11008983aff7fbdc8d35ac9d14a97
Andrii Petrov <andrew.petrov.dev@gmail.com>
2023-12-06 05:21:04 +0000

    Initial Commit

---
+++ TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by Andrew Petrov on 06.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
